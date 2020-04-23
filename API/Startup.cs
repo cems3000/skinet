f26e89c7bd3c0455bc88a10048668eb486d3b8cc
Carlos Eduardo Machanay Sulca <carlosmachanay@gmail.com>
2020-04-22 21:14:06 -0500

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,8 +37,14 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
 
-           
+                });
+            });
 
         }
 
@@ -57,6 +63,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
+using System.Linq;
 
 namespace Infrastructure.Data
 {
@@ -19,6 +20,24 @@ namespace Infrastructure.Data
 	  {
 	  	base.OnModelCreating(modelBuilder);
 	  	modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+	  	if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+	  	{
+	  		foreach (var entityType in modelBuilder.Model.GetEntityTypes()) 
+	  		{
+	  			var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType
+	  				== typeof(decimal));
+
+	  			foreach (var property in properties) 
+	  			{
+	  				modelBuilder.Entity(entityType.Name).Property(property.Name)
+	  					.HasConversion<double>();
+
+	  			}
+	  		}
+	  	}
+
 
 	  }
 
